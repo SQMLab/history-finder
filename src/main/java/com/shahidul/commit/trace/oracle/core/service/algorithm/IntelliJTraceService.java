13a9cb6647ade9d8cdd9f766ee1017833fac1f3f
Shahidul Islam <shahidul2k9@gmail.com>
2024-03-23 21:00:09 +0600

    store static trace to a separate file

---
+++ src/main/java/com/shahidul/commit/trace/oracle/core/service/algorithm/IntelliJTraceService.java
@@ -0,0 +1,49 @@
+package com.shahidul.commit.trace.oracle.core.service.algorithm;
+
+import com.shahidul.commit.trace.oracle.core.enums.TracerName;
+import com.shahidul.commit.trace.oracle.core.model.InputTrace;
+import com.shahidul.commit.trace.oracle.core.mongo.entity.AnalysisUdt;
+import com.shahidul.commit.trace.oracle.core.mongo.entity.CommitUdt;
+import com.shahidul.commit.trace.oracle.core.mongo.entity.TraceEntity;
+import com.shahidul.commit.trace.oracle.core.storage.StaticTraceDao;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Shahidul Islam
+ * @since 3/23/2024
+ */
+@AllArgsConstructor
+@Service
+public class IntelliJTraceService implements TraceService {
+    StaticTraceDao staticTraceDao;
+
+    @Override
+    public String getTracerName() {
+        return TracerName.INTELLI_J.getCode();
+    }
+
+    @Override
+    public String parseChangeType(String rawChangeType) {
+        return null;
+    }
+
+    @Override
+    public TraceEntity trace(TraceEntity traceEntity) {
+
+        InputTrace trace = staticTraceDao.findTrace(traceEntity.getOracleFileName(), TracerName.INTELLI_J);
+
+        List<CommitUdt> commitList = trace.getCommits()
+                .stream()
+                .map(commit -> CommitUdt.builder()
+                        .tracerName(getTracerName())
+                        .commitHash(commit.getCommitHash())
+                        .changeTags(commit.getChangeTags())
+                        .build())
+                .toList();
+        traceEntity.getAnalysis().put(getTracerName(), AnalysisUdt.builder().commits(commitList).build());
+        return traceEntity;
+    }
+}
