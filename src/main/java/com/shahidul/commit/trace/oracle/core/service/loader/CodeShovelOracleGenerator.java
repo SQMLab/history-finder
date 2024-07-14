0ed0b7fe6e55f49bb35d96d64d0b90f2a07b5acd
Shahidul Islam <shahidul2k9@gmail.com>
2024-07-14 23:57:03 +0600

    add oracle generator from code shovel trace

---
+++ src/main/java/com/shahidul/commit/trace/oracle/core/service/loader/CodeShovelOracleGenerator.java
@@ -0,0 +1,9 @@
+package com.shahidul.commit.trace.oracle.core.service.loader;
+
+/**
+ * @author Shahidul Islam
+ * @since 14/7/24
+ **/
+public interface CodeShovelOracleGenerator {
+    void generate();
+}
