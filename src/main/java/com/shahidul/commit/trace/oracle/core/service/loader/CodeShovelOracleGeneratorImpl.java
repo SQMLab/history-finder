ef7591a2f4d2dedddf1e88ef1f4edd6a23ea1692
Shahidul Islam <shahidul2k9@gmail.com>
2024-07-15 11:41:41 +0600

    sort traces by intended operation and method size

--- src/main/java/com/shahidul/commit/trace/oracle/core/service/loader/CodeShovelOracleGeneratorImpl.java
+++ src/main/java/com/shahidul/commit/trace/oracle/core/service/loader/CodeShovelOracleGeneratorImpl.java
@@ -2,30 +2,30 @@ package com.shahidul.commit.trace.oracle.core.service.loader;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.shahidul.commit.trace.oracle.cmd.writer.OutputFileWriter;
 import com.shahidul.commit.trace.oracle.config.AppProperty;
 import com.shahidul.commit.trace.oracle.config.CodeShovelOracleGeneratorConfiguration;
 import com.shahidul.commit.trace.oracle.config.RepositoryNameUrlMappingConfiguration;
-import com.shahidul.commit.trace.oracle.core.enums.TracerName;
+import com.shahidul.commit.trace.oracle.core.enums.ChangeTag;
+import com.shahidul.commit.trace.oracle.core.enums.LanguageType;
 import com.shahidul.commit.trace.oracle.core.model.InputCommit;
 import com.shahidul.commit.trace.oracle.core.model.InputOracle;
-import com.shahidul.commit.trace.oracle.core.model.InputTrace;
 import com.shahidul.commit.trace.oracle.core.mongo.dao.TraceDao;
 import com.shahidul.commit.trace.oracle.core.service.helper.OracleHelperService;
-import com.shahidul.commit.trace.oracle.core.service.helper.OracleHelperServiceImpl;
 import com.shahidul.commit.trace.oracle.util.Util;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.codetracker.api.MethodTracker;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Shahidul Islam
@@ -35,6 +35,13 @@ import java.util.stream.Stream;
 @Slf4j
 @AllArgsConstructor
 public class CodeShovelOracleGeneratorImpl implements CodeShovelOracleGenerator {
+    private static final Set<ChangeTag> DIFFICULT_CHANGE_TAG_SET = Set.of(
+            ChangeTag.PACKAGE,
+            ChangeTag.MOVE,
+            ChangeTag.SIGNATURE,
+            ChangeTag.FILE_RENAME,
+            ChangeTag.FILE_COPY,
+            ChangeTag.RENAME);
     RepositoryNameUrlMappingConfiguration repositoryMappingConfiguration;
     CodeShovelOracleGeneratorConfiguration codeShovelOracleGeneratorConfiguration;
     AppProperty appProperty;
@@ -46,13 +53,14 @@ public class CodeShovelOracleGeneratorImpl implements CodeShovelOracleGenerator 
     @Override
     public void generate() {
         try {
+            Map<String, String> startCommitHashMapping = getStartCommitHashMapping();
             File rootFileDir = new File(codeShovelOracleGeneratorConfiguration.getStoredTraceDirectory());
             AtomicInteger fileNo = new AtomicInteger(codeShovelOracleGeneratorConfiguration.getStartOracleFileId());
             Arrays.stream(rootFileDir.listFiles())
                     .filter(traceDirectory -> codeShovelOracleGeneratorConfiguration.getRepositoryList().contains(traceDirectory.getName()))
                     .forEach(traceDirectory -> {
-                        List<JsonNode> traceList = readAll(traceDirectory);
-                        List<JsonNode> topTraceList = selectTop(traceList, codeShovelOracleGeneratorConfiguration.getMaximumOraclePerRepository());
+                        List<JsonNode> traceList = readAll(traceDirectory, startCommitHashMapping);
+                        List<JsonNode> topTraceList = selectTop(traceList, codeShovelOracleGeneratorConfiguration.getMaximumOraclePerRepository(), traceDirectory.getName());
                         for (JsonNode jsonOracle : topTraceList) {
                             InputOracle inputOracle = convertToOracleFile(jsonOracle);
                             String fileName = generateFileName(fileNo.getAndIncrement(), inputOracle);
@@ -65,20 +73,47 @@ public class CodeShovelOracleGeneratorImpl implements CodeShovelOracleGenerator 
         }
     }
 
-    private List<JsonNode> readAll(File traceDirectory) {
+    private List<JsonNode> readAll(File traceDirectory, Map<String, String> startCommitHashMapping) {
         return Arrays.stream(Objects.requireNonNull(traceDirectory.listFiles()))
                 .map(traceFile -> {
                     try {
-                        return objectMapper.readValue(traceFile, JsonNode.class);
+                        JsonNode jsonNode = objectMapper.readValue(traceFile, JsonNode.class);
+                        if ("HEAD".equalsIgnoreCase(jsonNode.get("startCommitName").asText())) {
+                            ((ObjectNode) jsonNode).put("startCommitName", startCommitHashMapping.get(jsonNode.get("repositoryName").asText()));
+                        }
+                        return jsonNode;
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 })
-                .toList();
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    private List<JsonNode> selectTop(List<JsonNode> traceList, Integer maxLimit) {
-        return traceList.stream().limit(maxLimit).toList();
+    private List<JsonNode> selectTop(List<JsonNode> traceList, Integer maxLimit, String repositoryName) {
+
+
+        AtomicInteger totalIntendedOpsCount = new AtomicInteger();
+        AtomicInteger totalLineNumberCount = new AtomicInteger(0);
+        traceList.forEach(json -> {
+            Integer intendedOpsCount = countIntendedOps(json);
+            ((ObjectNode) json).put("intendedOpsCount", intendedOpsCount);
+            totalIntendedOpsCount.addAndGet(intendedOpsCount);
+
+            int lineNumberCount = countMethodLine(json);
+            ((ObjectNode) json).put("methodLineCount", lineNumberCount);
+            totalLineNumberCount.addAndGet(lineNumberCount);
+        });
+
+        double intendedOpsFactor = (totalIntendedOpsCount.get() == 0 ? 0 : 1.0 / totalIntendedOpsCount.get());
+        double lineNumberFactor = (totalLineNumberCount.get() == 0 ? 0 : 1.0 / totalLineNumberCount.get());
+        traceList.sort((x, y) -> {
+            double priorityX = x.get("intendedOpsCount").asInt() * intendedOpsFactor * 0.60 - x.get("methodLineCount").asInt() * lineNumberFactor * 0.40;
+            double priorityY = y.get("intendedOpsCount").asInt() * intendedOpsFactor * 0.60 - y.get("methodLineCount").asInt() * lineNumberFactor * 0.40;
+            return Double.compare(priorityX, priorityY) * -1;
+
+        });
+        return traceList.stream()
+                .limit(maxLimit).toList();
     }
 
     private InputOracle convertToOracleFile(JsonNode json) {
@@ -94,6 +129,7 @@ public class CodeShovelOracleGeneratorImpl implements CodeShovelOracleGenerator 
             return InputOracle.builder()
                     .repositoryName(repositoryName)
                     .repositoryUrl(repositoryMappingConfiguration.getRepositoryMapping().get(repositoryName))
+                    .language(LanguageType.JAVA.getCode())
                     .startCommitHash(json.get("startCommitName").asText())
                     .file(json.get("sourceFilePath").asText())
                     .elementType("method")
@@ -110,7 +146,46 @@ public class CodeShovelOracleGeneratorImpl implements CodeShovelOracleGenerator 
         }
     }
 
+
+    private Integer countIntendedOps(JsonNode json) {
+        List<InputCommit> commits = new ArrayList<>();
+        json.get("changeHistoryShort")
+                .fields()
+                .forEachRemaining(commit -> {
+                    commits.add(InputCommit.builder().commitHash(commit.getKey()).changeTags(Util.toChangeTags(commit.getValue().asText())).build());
+                });
+        AtomicInteger difficultChangeTagCount = new AtomicInteger(0);
+        commits.forEach(commit -> commit.getChangeTags().forEach(changeTag -> {
+            if (DIFFICULT_CHANGE_TAG_SET.contains(changeTag)) {
+                difficultChangeTagCount.incrementAndGet();
+            }
+        }));
+        return difficultChangeTagCount.get();
+    }
+
+    private int countMethodLine(JsonNode jsonNode) {
+        return jsonNode.get("functionEndLine").asInt() - jsonNode.get("functionStartLine").asInt() + 1;
+    }
+
     private String generateFileName(Integer fileId, InputOracle inputOracle) {
         return String.join("-", Util.formatOracleFileId(fileId), inputOracle.getRepositoryName(), Util.extractClassNameFromFile(inputOracle.getFile()), inputOracle.getElement()) + ".json";
+    }
+
+
+    private Map<String, String> getStartCommitHashMapping() {
+        Map<String, String> startCommitMapping = new HashMap<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(codeShovelOracleGeneratorConfiguration.getHeadPointerMappingFile());
+            List<String> lineList = IOUtils.readLines(fileInputStream, StandardCharsets.UTF_8);
+            fileInputStream.close();
+            lineList.forEach(line -> {
+                String[] parts = line.split("\t");
+                startCommitMapping.put(parts[0], parts[2]);
+            });
+        } catch (IOException e) {
+            log.error("Start commit hash mapping file parse error", e);
+            throw new RuntimeException(e);
+        }
+        return startCommitMapping;
     }
 }
