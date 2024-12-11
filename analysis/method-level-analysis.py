06d6cfe5786d5f6bb97b19bacec12906738a11ab
Shahidul Islam <shahidul2k9@gmail.com>
2024-12-11 16:58:02 -0600

    update box plot

--- analysis/method-level-analysis.py
+++ analysis/method-level-analysis.py
@@ -1,11 +1,15 @@
-from src.util import toUpperFirst
+import math
+
+import numpy as np
+
+from util import toUpperFirst
 import config
 
 trace = config.db["trace"]
 for dataset in config.datasetList:
     oracleName = dataset['oracleName']
-    print(f'{dataset["oracleName"]}', end='')
-    scoreMap = {tracerName : {'precision': 0, 'recall': 0, 'f1Score': 0, 'count': 0} for tracerName in config.tracerList}
+    print(f'{dataset["oracleName"]}')
+    scoreMap = {tracerName : {'precision': [], 'recall': [], 'f1Score': []} for tracerName in config.tracerList}
 
 
     for r in trace.find({'oracleFileId': {'$gte': dataset['range'][0], '$lte': dataset['range'][-1]}}, sort = [("oracleFileId", 1)], allow_disk_use = True).limit(1000):
@@ -13,10 +17,12 @@ for dataset in config.datasetList:
             for tracerName in config.tracerList:
                 if tracerName in r.get('analysis'):
                     analysis = r.get('analysis')[tracerName]
-                    scoreMap[tracerName]['precision'] += analysis['precision']
-                    scoreMap[tracerName]['recall'] += analysis['recall']
-                    #scoreMap[tracerName]['f1Score'] += analysis['f1Score']
-                    scoreMap[tracerName]['count'] += 1
+                    if math.isnan(analysis['precision']) is not True:
+                        scoreMap[tracerName]['precision'].append(analysis['precision'])
+                    if math.isnan(analysis['recall']) is not True:
+                        scoreMap[tracerName]['recall'].append(analysis['recall'])
+                    if math.isnan(analysis['f1Score']) is not True:
+                        scoreMap[tracerName]['f1Score'].append(analysis['f1Score'])
                 else:
                     #print(f'Warning: {oracleName} {dataset["range"]} {r.get("oracleFileId")} {tracerName} is not in analysis')
                     pass
@@ -25,6 +31,5 @@ for dataset in config.datasetList:
             print(f'Oracle File ID : {oracleFileId} \n{e}')
             raise e
     for tracerName in config.tracerList:
-        count = scoreMap[tracerName]['count']
-        print(f'    & {toUpperFirst(tracerName)} & {scoreMap[tracerName]["precision"]/count:.2f} & {scoreMap[tracerName]["recall"]/count:.2f} & {scoreMap[tracerName]["f1Score"]/count:.2f} \\\\')
+        print(f'    & {toUpperFirst(tracerName)} & {np.mean(np.array(scoreMap[tracerName]["precision"]))*100:.2f} & {np.mean(np.array(scoreMap[tracerName]["recall"]))*100:.2f} & {np.mean(np.array(scoreMap[tracerName]["f1Score"]))*100:.2f} \\\\')
     print(f'\\hline')
