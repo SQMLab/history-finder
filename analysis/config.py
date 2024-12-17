69ccf6feda62baaf2f6401fad9f9945976644242
Shahidul Islam <shahidul2k9@gmail.com>
2024-12-16 18:42:14 -0600

    add code for extracting history finder oracle project statistic like commit, star and fork count

--- analysis/config.py
+++ analysis/config.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://cto:cto123@localhost:27017/cto?authSource=admin")
+LOCAL_REPOSITORY_ROOT_DIRECTORY = '/home/cs/grad/islams32/dev/project/repository/'
+GITHUB_TOKEN = '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'
 db = client["cto"]
 datasetList = [{'oracleName': 'CodeShovel Training Dataset', 'oracleTye': 'Training', 'range': [1, 100]},
                {'oracleName': 'CodeShovel Testing Dataset', 'oracleTye': 'Testing', 'range': [101, 200]},
