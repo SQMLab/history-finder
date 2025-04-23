b1bb7d20b23ba2568e125dfb89e478ca4c19c6f8
Shahidul Islam <shahidul2k9@gmail.com>
2025-04-23 15:41:44 -0500

    add markers and haches

--- report-generator/execution-time-statistic.py
+++ report-generator/execution-time-statistic.py
@@ -8,17 +8,17 @@ runtimeStatisticsList = []
 tracerList = [t for t in config.tracerList if t != 'intelliJ']
 for dataset in config.datasetList:
     oracleName = dataset['oracleName']
-    runtimeStatistics = {tracerName : [] for tracerName in tracerList}
+    runtimeStatistics = {tracerName: [] for tracerName in tracerList}
 
-
-    for r in trace.find({'oracleFileId': {'$gte': dataset['range'][0], '$lte': dataset['range'][-1]}}, sort = [("oracleFileId", 1)], allow_disk_use = True).limit(1000):
+    for r in trace.find({'oracleFileId': {'$gte': dataset['range'][0], '$lte': dataset['range'][-1]}},
+                        sort=[("oracleFileId", 1)], allow_disk_use=True).limit(1000):
         try:
             for tracerName in tracerList:
                 if tracerName in r.get('analysis'):
                     analysis = r.get('analysis')[tracerName]
-                    runtimeStatistics[tracerName].append(analysis['runtime']/1000)
+                    runtimeStatistics[tracerName].append(analysis['runtime'] / 1000)
                 else:
-                    #print(f'Warning: {oracleName} {dataset["range"]} {r.get("oracleFileId")} {tracerName} is not in analysis')
+                    # print(f'Warning: {oracleName} {dataset["range"]} {r.get("oracleFileId")} {tracerName} is not in analysis')
                     pass
         except Exception as e:
             oracleFileId = r.get('oracleFileId')
@@ -32,15 +32,20 @@ for datasetIndex, runtimeStatistics in enumerate(runtimeStatisticsList):
     print(f'{datasetLabels[datasetIndex]}')
     for tracerIndex, tracerName in enumerate(tracerList):
         runtimes = np.array(runtimeStatistics[tracerName])
-        print(f'    & {toUpperFirst(tracerName)} & {np.mean(runtimes):.2f} & {np.median(runtimes):.2f} & {np.min(runtimes):.2f} & {np.max(runtimes):.2f} \\\\')
+        print(
+            f'    & {toUpperFirst(tracerName)} & {np.mean(runtimes):.2f} & {np.median(runtimes):.2f} & {np.min(runtimes):.2f} & {np.max(runtimes):.2f} \\\\')
     print(f'\\hline')
 
-boxColors = ['red', 'blue', 'green', 'yellow', 'purple'] #, 'orange'
+# boxplotColors = ['pink', 'lightgreen', 'lightblue', 'silver', 'peru']
+boxplotColors = ['#d9a999', '#80c080', '#c080c0', '#a8a8a8', '#e0b88f']
+cdfPlotColors = ['brown', 'green', 'purple', 'dimgray', 'peru']
 
-
+HATCHES = ['xx', '//', '.', 'O.', '*']
+MARKERS = ['h', 'd', 'x', '>', '*']
+LINE_STYLES = [':', '--', '-.', '-', (0, (4, 2, 1, 2))]
 datasetLength = len(datasetLabels)
 # Create a box plot
-boxPlotFigure, boxPlotAxes = plt.subplots(1, datasetLength, figsize=(12, 4), constrained_layout = True, sharey=False)
+boxPlotFigure, boxPlotAxes = plt.subplots(1, datasetLength, figsize=(12, 4), constrained_layout=True, sharey=False)
 cdfFigure, cdfAxes = plt.subplots(1, datasetLength, figsize=(12, 4), sharey=False)
 boxPlotRuntimeLimit = [40, 15, 50]
 cdfPlotRuntimeLimitAndStepSize = [[40, 5], [30, 5], [80, 10]]
@@ -48,22 +53,33 @@ for datasetIndex, runtimeStatistics in enumerate(runtimeStatisticsList):
     boxPlot = boxPlotAxes[datasetIndex]
     cdfPlot = cdfAxes[datasetIndex]
     boxPlot.set_ylim(0, boxPlotRuntimeLimit[datasetIndex])
+    step = 10
     for tracerIndex, tracerName in enumerate(tracerList):
         runtimes = runtimeStatistics[tracerName]
         runtimes = np.sort(runtimes)
-        label = toUpperFirst(tracerName) if datasetIndex + 1 == datasetLength  else ''
+        label = toUpperFirst(tracerName) if datasetIndex + 1 == datasetLength else ''
         boxPlot.boxplot(
             runtimes,
-            label= label,
+            label=label,
             positions=[tracerIndex + 1],
             patch_artist=True,
             widths=0.5,
-            boxprops=dict(facecolor=boxColors[tracerIndex])
+            boxprops=dict(facecolor=boxplotColors[tracerIndex], hatch=HATCHES[tracerIndex]),
+            medianprops=dict(color='black', linewidth=1.5)
         )
-        print(runtimes,  end=',')
+        # step += tracerIndex + 1
+        print(runtimes, end=',')
         # Set x-axis labels and title
         cdf = np.arange(1, len(runtimes) + 1) / len(runtimes)
-        cdfPlot.plot(runtimes, cdf, label = label,  color=boxColors[tracerIndex])
+
+        x_sub = runtimes[tracerIndex + 10::step]
+        y_sub = cdf[tracerIndex + 10::step]
+        # Append last point if it's not already included
+        if runtimes[-1] > x_sub[-1]:
+            x_sub = np.append(x_sub, runtimes[-1])
+            y_sub = np.append(y_sub, cdf[-1])
+        cdfPlot.plot(x_sub, y_sub, label=label, color=cdfPlotColors[tracerIndex], linewidth=1.5,
+                     linestyle=LINE_STYLES[tracerIndex], marker=MARKERS[tracerIndex])
 
     # boxPlot.set_xticks([tracerIndex + 1 for tracerIndex in range(len(tracerList))])
     # boxPlot.set_xticklabels([toUpperFirst(tracerName) for tracerName in tracerList], rotation=45, ha="right")
@@ -72,7 +88,8 @@ for datasetIndex, runtimeStatistics in enumerate(runtimeStatisticsList):
     cdfPlot.set_xlim(0, cdfPlotRuntimeLimitAndStepSize[datasetIndex][0])
     cdfPlot.set_title(datasetLabels[datasetIndex])
     cdfPlot.set_yticks(np.arange(0, 1.1, 0.1))
-    cdfPlot.set_xticks(np.arange(0, cdfPlotRuntimeLimitAndStepSize[datasetIndex][0] + 1, cdfPlotRuntimeLimitAndStepSize[datasetIndex][1]))
+    cdfPlot.set_xticks(np.arange(0, cdfPlotRuntimeLimitAndStepSize[datasetIndex][0] + 1,
+                                 cdfPlotRuntimeLimitAndStepSize[datasetIndex][1]))
     cdfPlot.grid(axis='both', linestyle='--', alpha=0.7)
     boxPlot.legend()
     boxPlot.set_xticks([])
@@ -106,6 +123,7 @@ cdfFigure.supxlabel('Execution Time (s)')
 # # Add grid for better readability
 # ax.grid(axis='y', linestyle='--', alpha=0.7)
 
+plt.savefig("../cache/execution-time-box-plot.png", dpi=300, bbox_inches='tight')
 # Show the plot
 plt.tight_layout()
 plt.show()
