82d5ac0e2ecf68f7cf12397a15aa7c15ff24a0a3
Shahidul Islam <shahidul2k9@gmail.com>
2025-05-06 12:27:17 -0500

    draw oracle CDF

---
+++ report-generator/oracle-commit-count-cdf.py
@@ -0,0 +1,111 @@
+import os
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+from util import isWeakCommit, toUpperFirst
+
+baseDirectorMap = {
+    'codeShovel': "../src/main/resources/stubs/input/",
+    'codeTracker': '/home/cs/grad/islams32/dev/rnd/history finder/oracle/codetracker/',
+    'codeShovelNew': "../src/main/resources/oracle/",
+    'historyFinder': "../src/main/resources/oracle/"
+}
+
+
+fileMap = {oracleKey : [os.path.join(baseDirectory, f) for f in os.listdir(baseDirectory) if
+                      os.path.isfile(os.path.join(baseDirectory, f))] for oracleKey, baseDirectory in baseDirectorMap.items()}
+fileMap['codeShovelNew'] = list(sorted(filter(lambda f: int(f.split('/')[-1].split('-')[0]) <= 200, fileMap['codeShovelNew'])))
+fileMap['historyFinder'] = list(sorted(filter(lambda f: 201 <= int(f.split('/')[-1].split('-')[0]) <= 400, fileMap['historyFinder'])))
+
+commitCountMap = {
+    key : [] for key in fileMap.keys()
+}
+for oracleKey in fileMap:
+    for file in fileMap[oracleKey]:
+        commitSet = set()
+        jsonFile = json.load(open(file, 'r'))
+        match oracleKey:
+            case 'codeShovel':
+                commitSet |= jsonFile['expectedResult'].keys()
+            case 'codeTracker':
+                for changeItem in jsonFile['expectedChanges']:
+                    commitSet.add(changeItem['commitId'])
+            case 'codeShovelNew' | 'historyFinder':
+                for changeItem in jsonFile['commits']:
+                    commitSet.add(changeItem['commitHash'])
+        commitCountMap[oracleKey].append(max(len(commitSet) - 1, 0))
+
+
+boxplotColors = ['#d9a999', '#80c080', '#c080c0', '#a8a8a8', '#e0b88f']
+cdfPlotColors = ['brown', 'green', 'purple', 'dimgray', 'peru']
+
+HATCHES = ['xx', '//', '.', 'O.', '*']
+MARKERS = ['h', 'd', 'x', '>', '*']
+LINE_STYLES = [':', '--', '-.', '-', (0, (4, 2, 1, 2))]
+cdfFigure, cdfAxes = plt.subplots(1, 1, figsize=(10, 10), sharey=False)
+for subplotIndex in range(1):
+    cdfPlot = cdfAxes[subplotIndex] if isinstance(cdfAxes,list) else cdfAxes
+
+    step = 10
+    cdfIndex = 0
+    for oracleKey, commitCountSeq in commitCountMap.items():
+        commitCounts = list(commitCountSeq)
+        commitCounts = np.sort(commitCounts)
+        label = toUpperFirst(oracleKey) if subplotIndex == 0 else ''
+
+        # step += tracerIndex + 1
+        print(commitCounts, end=',')
+        # Set x-axis labels and title
+        cdf = np.arange(1, len(commitCounts) + 1) / len(commitCounts)
+
+        x_sub = commitCounts[cdfIndex + 10::step]
+        y_sub = cdf[cdfIndex + 10::step]
+        # x_sub = commitCounts[::]
+        # y_sub = cdf[::]
+
+        cdfPlot.plot(x_sub, y_sub, label=label, color=cdfPlotColors[cdfIndex], linewidth=3,
+                     markersize=10,
+                     markeredgewidth=2,
+                     linestyle=LINE_STYLES[cdfIndex], marker=MARKERS[cdfIndex])
+        cdfIndex += 1
+
+    # cdfPlot.set_xlim(0, cdfPlotRuntimeLimitAndStepSize[datasetIndex][0])
+    # cdfPlot.set_title(datasetLabels[datasetIndex])
+    # cdfPlot.set_yticks(np.arange(0, 1.1, 0.1))
+    # cdfPlot.set_xticks(np.arange(0, cdfPlotRuntimeLimitAndStepSize[datasetIndex][0] + 1,
+    #                              cdfPlotRuntimeLimitAndStepSize[datasetIndex][1]))
+    cdfPlot.grid(axis='both', linestyle='--', alpha=0.5)
+    # boxPlot.legend()
+    if subplotIndex == 0:
+        cdfPlot.set_ylabel("CDF")
+        cdfPlot.legend(title="Oracles", loc='lower right')
+
+
+cdfFigure.supxlabel('Number of revisions of method')
+#
+# # Apply consistent colors to boxes
+# for i, patch in enumerate(box['boxes']):
+#     patch.set_facecolor(boxColors[i % len(boxColors)])  # Cycle through colors
+#
+# # Set x-axis labels for groups with spacing
+# ax.set_xticks(datasetLabelPositions)
+# ax.set_xticklabels(datasetLabels, rotation=45, ha="right")
+#
+# # Add legend for color mapping
+# for i, color in enumerate(boxColors):
+#     ax.plot([], [], color=color, label=f'{toUpperFirst(tracerList[i])}')
+
+# # Add legend, title, and axis labels
+# ax.legend(title="Tools", bbox_to_anchor=(1.05, 1), loc='upper left')
+# ax.set_title("Method Tracking Execution time in seconds")
+# ax.set_xlabel("Dataset")
+# ax.set_ylabel("Time (s)")
+#
+# # Add grid for better readability
+# ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+cdfFigure.savefig("../cache/method-revisions-cdf.png", dpi=300, bbox_inches='tight')
+# Show the plot
+plt.tight_layout()
+plt.show()
